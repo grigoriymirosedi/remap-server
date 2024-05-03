e626f6436349229553f938155fba5ada22b8be8f
Grigoriy <92877083+grigoriymirosedi@users.noreply.github.com>
2024-05-04 01:07:13 +0300

    fixing

--- src/main/kotlin/com/example/features/recycle_point/RecyclePointRouting.kt
+++ src/main/kotlin/com/example/features/recycle_point/RecyclePointRouting.kt
@@ -1,6 +1,7 @@
 package com.example.features.recycle_point
 
 import com.example.utils.convertToCategoryId
+import com.example.utils.toCategoryId
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
@@ -14,7 +15,7 @@ fun Application.configureRecyclePointRouting() {
         post("/v1/recycle-points") {
             val categoryType: List<String> = call.request.queryParameters.getAll("categoryType") ?: emptyList()
             if(!categoryType.isEmpty()) {
-                val categoryId = categoryType.convertToCategoryId()
+                val categoryId = categoryType.map{ it.toCategoryId() }
                 RecyclePointController(call).createRecyclePoint(categoryId = categoryId)
             } else {
                 call.respondText("Missing categoryType value!", status = HttpStatusCode.NoContent)
